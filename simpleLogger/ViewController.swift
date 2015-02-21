320d82a5ab591786cb9f881e303bb432fa08c37a
xdevx32 <angelkukushev333@gmail.com>
2015-02-21 18:19:15 +0200

    Classes implemented, console log works, default log works , need more work on other methods and classes.

--- simpleLogger/ViewController.swift
+++ simpleLogger/ViewController.swift
@@ -8,6 +8,32 @@
 
 import UIKit
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //base case test
+        //let logger = HTTPLogger(level: 1, message: "HelloHTTP")
+        let consoleLogger = ConsoleLogger(level: 1, message: "Hello Console")
+        consoleLogger.consoleLog(consoleLogger.level, message: consoleLogger.message)
+        
+        let myDefaultLogger = MyLogger(level: 2, message: "Hello World")
+        messageLabel.text = myDefaultLogger.log(myDefaultLogger.level, message: myDefaultLogger.message)
+
+        
+        // console logger test
+       // let consoleLogger = ConsoleLogger()
+       // consoleLogger.consoleLog(1, message: "ConsoleLogger")
+        
+        //HTTPLogger test
+      ///  let htLogger = HTTPLogger(level: 1,message: "Hello World"
+       // htLogger.log(1, message: "HTTPPPPP")
+    }
+}
+
+
 extension NSDate {
     var formatted: String {
         let formatter = NSDateFormatter()
@@ -20,20 +46,26 @@ extension NSDate {
 
 class MyLogger{
     
-    func log(var level: Int, message: String) -> String{
+    var level: Int = 0
+    var message: String = ""
+    
+    init (level: Int, message: String){
+    self.level = level
+    self.message = message
+    }
+    
+    func log(level: Int, message: String) -> String{
         
         var levelMsg: String
-        
-        if(level == 1){
+    
+        switch level {
+        case 1:
             levelMsg = "INFO"
-        }
-        else if(level == 2){
+        case 2:
             levelMsg = "WARNING"
-        }
-        else if(level == 3){
+        case 3:
             levelMsg = "PLSCHECKFFS"
-        }
-        else{
+        default:
             levelMsg = "WRONG LEVEL INPUT"
         }
         
@@ -48,31 +80,43 @@ class MyLogger{
 
 }
 
-class ViewController: UIViewController {
+class ConsoleLogger: MyLogger {
     
-    @IBOutlet weak var messageLabel: UILabel!
+   func consoleLog(var level: Int, message: String){
+        println(log(level, message: message))
+    }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let logger = MyLogger()
-        logger.log(1, message: "Whassup")
-        
-      //  messageLabel.text = log(2, message: "Hello World")
-      
-    }
-    
-    
-    func HTTPLogger(var level: Int, message: String) -> String{
-        
-        
-        
-        
-        return "test"
-    }
-    
-    func consoleLogger(){
-        
+class FileLogger: MyLogger {
+  func fileLog(var level: Int, message: String) -> String{
+        return "FileLoggerTest"
     }
     
 }
 
+class HTTPLogger: MyLogger {
+    override func log(var level: Int, message: String) -> String{
+        println("Hey, I am here in the HTTPLogger log function")
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://www.thisismylink.com/postName.php")!)
+        request.HTTPMethod = "POST"
+        let postString = "id=13&name=Jack"
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil {
+                println("error=\(error)")
+                return
+            }
+            
+            println("response = \(response)")
+            
+            let responseString = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println("responseString = \(responseString)")
+        }
+        task.resume()
+        return "HTTPLoggerTest"
+    }
+    
+    
+}
